3cb431cad62eaab650fd72ed570f559829aae9da
tugba-koc <tugba.koc@sisal.com>
2023-12-19 21:39:59 +0300

    create basic structure

---
+++ be/src/main/java/dev/tugba/taskapp/TaskAppApplication.java
@@ -0,0 +1,13 @@
+package dev.tugba.taskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAppApplication.class, args);
+	}
+
+}
