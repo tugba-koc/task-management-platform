1c1f3fe08fbb49e9fc8c819fabafd8e801565743
tugba-koc <koctug@itu.edu.tr>
2023-12-24 16:40:48 +0300

    feat: add alias to vite

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@layout': path.resolve(__dirname, './src/layout'),
+      '@redux': path.resolve(__dirname, './src/redux'),
+      '@types': path.resolve(__dirname, './src/types'),
+    },
+  },
   plugins: [react()],
-})
+});
